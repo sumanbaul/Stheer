763e4f6881de9218ca140029b84d5fdcd8da2555
Suman Baul <musique.suman@gmail.com>
2022-03-06 23:32:40 +0530

    floating button update

--- lib/widgets/buttons/listerFloatingButton.dart
+++ lib/widgets/buttons/listerFloatingButton.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ListerFloatingButton extends StatelessWidget {
-  const ListerFloatingButton({Key key}) : super(key: key);
-
+  const ListerFloatingButton({Key key, this.onClicked, this.floaterIcon})
+      : super(key: key);
+  final VoidCallback onClicked;
+  final Icon floaterIcon;
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
@@ -10,8 +12,9 @@ class ListerFloatingButton extends StatelessWidget {
       splashColor: Color(0xff94bbe9),
       hoverColor: Color(0xffeeaeca),
       focusColor: Color(0xff94bbe9),
-      // onPressed: started ? stopListening : startListening,
-      // tooltip: 'Start/Stop sensing',
+      onPressed: onClicked, //started ? stopListening : startListening,
+      tooltip: 'Start/Stop sensing',
+      child: floaterIcon,
       // child: _loading
       //     ? Icon(Icons.close)
       //     : (started ? Icon(Icons.close) : Icon(Icons.play_arrow)),
